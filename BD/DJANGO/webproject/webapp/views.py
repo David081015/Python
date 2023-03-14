f53bc59a17ac0f25ecdd1a42720671155f1aa093
David081015 <byjoker19.play@gmail.com>
2023-03-14 07:54:17 -0500

    Django

---
+++ BD/DJANGO/webproject/webapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from personas.models import Persona
+# Create your views here.
+def index(request):
+    return render(request,'Bienvenido.html')
+
+def indexPersonas(request):
+    noPersonas = Persona.objects.count()
+    personas = Persona.objects.order_by('id')
+    return render(request,'indexPersonas.html',{'noPersonas':noPersonas, 'personas':personas})
